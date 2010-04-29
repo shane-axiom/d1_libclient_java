3728e10d9970591ea0f655549516adb325bd6edf
Matt Jones <jones@nceas.ucsb.edu>
2010-04-29 08:35:45 +0000

    Refactored to remove method that is now in TestUtiltiies.

--- src/test/java/org/dataone/client/D1ClientTest.java
+++ src/test/java/org/dataone/client/D1ClientTest.java
@@ -213,29 +213,6 @@ public class D1ClientTest extends TestCase {
         assertTrue(1==1);
     }
 
-//    /** Generate a timestamp for use in IDs. */
-//    private String generateTimeString()
-//    {
-//        StringBuffer guid = new StringBuffer();
-//
-//        // Create a calendar to get the date formatted properly
-//        String[] ids = TimeZone.getAvailableIDs(-8 * 60 * 60 * 1000);
-//        SimpleTimeZone pdt = new SimpleTimeZone(-8 * 60 * 60 * 1000, ids[0]);
-//        pdt.setStartRule(Calendar.APRIL, 1, Calendar.SUNDAY, 2 * 60 * 60 * 1000);
-//        pdt.setEndRule(Calendar.OCTOBER, -1, Calendar.SUNDAY, 2 * 60 * 60 * 1000);
-//        Calendar calendar = new GregorianCalendar(pdt);
-//        Date trialTime = new Date();
-//        calendar.setTime(trialTime);
-//        guid.append(calendar.get(Calendar.YEAR));
-//        guid.append(calendar.get(Calendar.DAY_OF_YEAR));
-//        guid.append(calendar.get(Calendar.HOUR_OF_DAY));
-//        guid.append(calendar.get(Calendar.MINUTE));
-//        guid.append(calendar.get(Calendar.SECOND));
-//        guid.append(calendar.get(Calendar.MILLISECOND));
-//
-//        return guid.toString();
-//    }
-
     /** Generate a SystemMetadata object with bogus data. */
     private static SystemMetadata generateSystemMetadata(Identifier guid, ObjectFormat objectFormat) {
         SystemMetadata sysmeta = new SystemMetadata();
