769b7d53cc1bef1a39096b1ecd382c6d9e03597c
Chris Jones <cjones@nceas.ucsb.edu>
2011-06-06 19:08:55 +0000

    Minor change - added documentation to the ObjectFormatCache test class.

--- src/test/java/org/dataone/client/ObjectFormatCacheTest.java
+++ src/test/java/org/dataone/client/ObjectFormatCacheTest.java
@@ -1,5 +1,26 @@
-package org.dataone.client;
+/**
+ * This work was created by participants in the DataONE project, and is
+ * jointly copyrighted by participating institutions in DataONE. For
+ * more information on DataONE, see our web site at http://dataone.org.
+ *
+ *   Copyright ${year}
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * $Id$
+ */
 
+package org.dataone.client;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -8,22 +29,16 @@ import org.dataone.client.ObjectFormatCache;
 import org.dataone.service.exceptions.NotFound;
 import org.dataone.service.types.ObjectFormatList;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Test the ObjectFormatCache to retrieve the object format list, a single 
+ * object format, and test a known bad format to handle the NotFound exception.
+ * @author cjones
+ *
+ */
 public class ObjectFormatCacheTest {
 	
-	private static String fmtidStr = "text/csv";
-	
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
   @Test
   public void testHarnessCheck() {
       assertTrue(true);
