f702808b50d7696905c2253afe4b0fff3fe825e3
Chris Jones <cjones@nceas.ucsb.edu>
2011-06-09 01:18:45 +0000

    Changed the ObjectFormatCache to extend ObjectFormatServiceImpl, and updated the ObjectFormatCacheTest to now use getInstance() since listFormats() and getFormat() are no longer static.  Updated MNode and DataoneEMLParser accordingly.  D1Object needs updating, but is not compiling due to changes to MNode now implementing the new API. I assume those issues are being addressed now by those making the changes.

--- src/main/java/org/dataone/client/ObjectFormatCache.java
+++ src/main/java/org/dataone/client/ObjectFormatCache.java
@@ -25,12 +25,12 @@ package org.dataone.client;
 import java.util.HashMap;
 
 import org.apache.log4j.Logger;
-import org.dataone.service.ObjectFormatDiskCache;
 import org.dataone.service.exceptions.InsufficientResources;
 import org.dataone.service.exceptions.InvalidRequest;
 import org.dataone.service.exceptions.NotFound;
 import org.dataone.service.exceptions.NotImplemented;
 import org.dataone.service.exceptions.ServiceFailure;
+import org.dataone.service.impl.ObjectFormatServiceImpl;
 import org.dataone.service.types.ObjectFormat;
 import org.dataone.service.types.ObjectFormatIdentifier;
 import org.dataone.service.types.ObjectFormatList;
@@ -44,7 +44,7 @@ import org.dataone.service.types.ObjectFormatList;
  * @author cjones
  *
  */
-public class ObjectFormatCache {
+public class ObjectFormatCache extends ObjectFormatServiceImpl {
   
   /* The instance of the logging class */
   private static Logger logger = Logger.getLogger(ObjectFormatCache.class.getName());
@@ -58,30 +58,34 @@ public class ObjectFormatCache {
   /* The searchable map of object formats */
   private HashMap<String, ObjectFormat> objectFormatMap;
   
-  /* The fallback object format list from the disk cache */
-  private ObjectFormatDiskCache objectFormatDiskCache;
-  
   /**
    * Constructor: Creates an instance of the object format service using the
    * the given Coordinating Node URL to load the authoritative object format
    * list.
    * 
    * @param cnURL - the HTTP URL to the Coordinating Node to query
+   * @throws ServiceFailure 
    */
   private ObjectFormatCache() {
+  	
+  	super();
         
-  	try {
-      
   	  // refresh the object format list
-      populateObjectFormatList();
-	
-  	} catch (ServiceFailure sf ) {
-  	  
-  		logger.error("There was a problem creating the ObjectFormatCache. " +
-  				        "The error message was: " + sf.getMessage());
-  		// TODO: how should this be propagated since it is essentially fatal?
-  	}
-           
+      try {
+	      getCachedList();
+      
+      } catch (InvalidRequest e) {
+          
+      } catch (NotFound e) {
+
+      } catch (InsufficientResources e) {
+
+      } catch (NotImplemented e) {
+
+      } catch (ServiceFailure e) {
+
+      }
+	           
   }
   
   /*
@@ -111,65 +115,44 @@ public class ObjectFormatCache {
   	return objectFormatMap;
   	
   }
-  
-  /**
-
-   * Populate the ObjectFormatCache's objectFormatList from the cached list.
-   * 
-   * @throws ServiceFailure
-   * @throws NotImplemented 
-   * @throws InsufficientResources 
-   * @throws NotFound 
-   * @throws InvalidRequest 
-   */
-  private  void populateObjectFormatList()
-    throws ServiceFailure {
     
-  	// get the backup cache instance if needed
-  	objectFormatDiskCache = new ObjectFormatDiskCache();
-
+  /**
+   * 
+   * @return objectFormatList - the list of object formats
+   * @throws InvalidRequest
+   * @throws ServiceFailure
+   * @throws NotFound
+   * @throws InsufficientResources
+   * @throws NotImplemented
+   */
+  private ObjectFormatList getCachedList() 
+    throws InvalidRequest, ServiceFailure,
+    NotFound, InsufficientResources, NotImplemented {
+	  
     try {
-  		
-			// create the searchable map of object formats
-      objectFormatList = this.getListFromCN();
+	    // Get the reference to the CN
+	    CNode cn = D1Client.getCN();
+	    
+	    // get the object format list from the CN
+	    objectFormatList = cn.listFormats();
+    
+    } catch (InvalidRequest e) {
+      objectFormatList = ObjectFormatServiceImpl.getInstance().listFormats();
       
-    } catch (ServiceFailure se) {
+    } catch (ServiceFailure e) {
+      objectFormatList = ObjectFormatServiceImpl.getInstance().listFormats();
 
-  		logger.error("There was a problem creating the ObjectFormatCache. "    +
-	        "Reverting to the on-disk cache version.  The error message was: " + 
-	        se.getMessage());
-      objectFormatList = ObjectFormatDiskCache.listFormats();
-        
-    } catch ( NotFound nfe ) {
-  	  
-  		logger.error("There was a problem creating the ObjectFormatCache. "    +
-	        "Reverting to the on-disk cache version.  The error message was: " + 
-	        nfe.getMessage());
-  		objectFormatList = ObjectFormatDiskCache.listFormats();
+    } catch (NotFound e) {
+      objectFormatList = ObjectFormatServiceImpl.getInstance().listFormats();
 
-    } catch ( InvalidRequest ire ) {
-      
-  		logger.error("There was a problem creating the ObjectFormatCache. "    +
-	        "Reverting to the on-disk cache version.  The error message was: " + 
-	        ire.getMessage());
-  		objectFormatList = ObjectFormatDiskCache.listFormats();
+    } catch (InsufficientResources e) {
+      objectFormatList = ObjectFormatServiceImpl.getInstance().listFormats();
 
-    } catch ( InsufficientResources isre ) {
-      
-  		logger.error("There was a problem creating the ObjectFormatCache. "    +
-	        "Reverting to the on-disk cache version.  The error message was: " + 
-	        isre.getMessage());
-  		objectFormatList = ObjectFormatDiskCache.listFormats();
-
-    } catch ( NotImplemented nie ) {
-      
-  		logger.error("There was a problem creating the ObjectFormatCache. "    +
-	        "Reverting to the on-disk cache version.  The error message was: " + 
-	        nie.getMessage());
-  		objectFormatList = ObjectFormatDiskCache.listFormats();
+    } catch (NotImplemented e) {
+      objectFormatList = ObjectFormatServiceImpl.getInstance().listFormats();
 
     }
-
+  	
     // index the object format list based on the format identifier
     int listSize = objectFormatList.sizeObjectFormats();
     
@@ -181,80 +164,72 @@ public class ObjectFormatCache {
       getObjectFormatMap().put(identifier, objectFormat);
       
     }
-    
-  }
-  
-  /**
-   * 
-   * @return objectFormatList - the list of object formats
-   * @throws ServiceFailure
-   */
-  private  ObjectFormatList getListFromCN() 
-    throws InvalidRequest, ServiceFailure, NotFound, InsufficientResources,
-    NotImplemented {
-	  
-  	try {
-	    
-    	// Get the reference to the CN
-	    CNode cn = D1Client.getCN();
-	    
-	    // get the object format list from the CN
-	    objectFormatList = cn.listFormats();
-    
-  	} catch (ServiceFailure e) {
-    	throw e;
-    	
-    }
-  	
+
 		return objectFormatList;
 
   }
   
   /**
-   * List the object formats registered with the object format service.
-   * 
-   * @return objectFormatList - the list of object formats
-   */
-  public static ObjectFormatList listFormats() {
-    
-		return getInstance().objectFormatList;
-    
-  }
-  
-  /**
    * Get the object format based on the given identifier.
    * 
-   * @param format - the object format identifier
+   * @param fmtid - the object format identifier
    * @return objectFormat - the ObjectFormat represented by the format identifier
+   * @throws InvalidRequest 
+   * @throws ServiceFailure 
    * @throws NotFound 
+   * @throws InsufficientResources 
+   * @throws NotImplemented 
    */
-  public static ObjectFormat getFormat(ObjectFormatIdentifier fmtid) 
-    throws NotFound {
+	@Override
+	public ObjectFormat getFormat(ObjectFormatIdentifier fmtid)
+	  throws InvalidRequest, ServiceFailure, NotFound, InsufficientResources,
+	  NotImplemented {
     
     return getFormat(fmtid.getValue());
-            
-  }
+    
+	}
 
-  /**
+	/**
    * Get the object format based on the given identifier string.
    * 
    * @param format - the object format identifier string
    * @return objectFormat - the ObjectFormat represented by the format identifier
    * @throws NotFound 
    */
-  public static ObjectFormat getFormat(String fmtidStr) 
+  public ObjectFormat getFormat(String fmtidStr) 
     throws NotFound {        	
     
   	ObjectFormat objectFormat = null;
   	
-    objectFormat = getInstance().getObjectFormatMap().get(fmtidStr);
+    objectFormat = getObjectFormatMap().get(fmtidStr);
     
     if ( objectFormat == null ) {
       
-    	throw new NotFound("4848", "The format specified by " + fmtidStr + 
-    			               " does not exist at this node.");
+    	try {
+	      getCachedList();
+      } catch (InvalidRequest e) {
+      	throw new NotFound("4848", "The format specified by " + fmtidStr + 
+        " does not exist at this node.");
+        
+      } catch (ServiceFailure e) {
+      	throw new NotFound("4848", "The format specified by " + fmtidStr + 
+        " does not exist at this node.");
+        
+      } catch (NotFound e) {
+      	throw e;
+      
+      } catch (InsufficientResources e) {
+      	throw new NotFound("4848", "The format specified by " + fmtidStr + 
+        " does not exist at this node.");
+      
+      } catch (NotImplemented e) {
+      	throw new NotFound("4848", "The format specified by " + fmtidStr + 
+        " does not exist at this node.");
+      
+      }
+     
     }
-    
+     
     return objectFormat;
     
   }
