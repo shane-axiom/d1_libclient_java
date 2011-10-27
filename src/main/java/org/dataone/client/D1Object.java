c61662f70a579bdcb981aed5045b45bb6bcd689c
Matt Jones <jones@nceas.ucsb.edu>
2011-10-27 21:34:40 +0000

    Merged changes from d1_libclient_java_package_refactor branch.  Now D1Object and DataPackage have partial support for ORE documents, replacing the obsolete describes/describedBy system metadata properties.  Work is incomplete -- still need to fully handle ORE documents.  D1Object constructor has changed, which will likely affect how to construct these objects in client code.

--- src/main/java/org/dataone/client/D1Object.java
+++ src/main/java/org/dataone/client/D1Object.java
@@ -63,14 +63,16 @@ import org.dataone.service.types.v1.util.ChecksumUtil;
 public class D1Object {
 
     private SystemMetadata sysmeta;
-    // the following 4 will probably be replaced by ResourceMap;
-    private List<Identifier> describesList;
-    private List<Identifier> describedByList;
-    private List<Identifier> obsoletesList;
-    private List<Identifier> obsoletedByList;
     
     // TODO: this should also be able to be a reference to data, rather than a value, with late binding to allow efficient implementations
     private byte[] data;
+    
+    /**
+     * Construct a new D1Object, which is then populated with data and system metadata
+     * during the download process.
+     */
+    private D1Object() {
+    }
     
     /**
      * Create an object that contains the system metadata and data bytes from the D1 system. The identifier
@@ -78,9 +80,9 @@ public class D1Object {
      * id are downloaded from the resolved Member Node and stored in the D1Object instance for easy retrieval.
      * @param id the Identifier to be retrieved from D1
      */
-    public D1Object(Identifier id) {
-        download(id);
-    }
+//    public D1Object(Identifier id) {
+//        download(id);
+//    }
 
     /**
      * Create an object that contains the given data bytes and with the given system metadata values. This 
@@ -92,16 +94,15 @@ public class D1Object {
      * @param format the format of the object
      * @param submitter the submitter for the object
      * @param nodeId the identifier of the node on which the object will be created
-     * @param describes the list of identifiers that this object describes
-     * @param describedBy the list of objects described by this object
      * @throws NoSuchAlgorithmException if the checksum algorithm does not exist
      * @throws IOException if the data bytes can not be read
      * @throws NotFound 
+     * @throws InvalidRequest 
      */
-    public D1Object(Identifier id, byte[] data, String format, String submitter, String nodeId, 
-            String[] describes, String[] describedBy) throws NoSuchAlgorithmException, IOException, NotFound {
+    public D1Object(Identifier id, byte[] data, String format, String submitter, String nodeId) 
+        throws NoSuchAlgorithmException, IOException, NotFound, InvalidRequest {
         this.data = data;
-        this.sysmeta = generateSystemMetadata(id, data, format, submitter, nodeId, describes, describedBy);
+        this.sysmeta = generateSystemMetadata(id, data, format, submitter, nodeId);
     }
 
     /**
@@ -148,26 +149,6 @@ public class D1Object {
         this.data = data;
     }
 
-    /**
-     * @return the list of objects this describes
-     */
-    public List<Identifier> getDescribeList() {
-        return this.describesList;
-    }
-    
-    /**
-     * @return the list of objects that describe this
-     */
-    public List<Identifier> getDescribeByList() {
-        return this.describedByList;
-    }
-
-    /**
-     * @return the list of objects that obsolete this
-     */
-    public List<Identifier> getObsoletedByList() {
-        return this.obsoletedByList;
-    }
     /**
      * Create the object on the associated member node that is present in the system metadata
      * for the D1Object. Assumes that the object has not already been created.  If it
@@ -234,11 +215,10 @@ public class D1Object {
      * Contact D1 services to download the metadata and data.
      * @param id identifier to be downloaded
      */
-    private void download(Identifier id) {
-        try {
-
-	    	D1Object o = null;
-	    
+    public static D1Object download(Identifier id) {
+        
+        D1Object o = null;
+        try {	    
 	        CNode cn = D1Client.getCN();
 	        Session token = new Session();
 	    
@@ -246,7 +226,8 @@ public class D1Object {
             // Get the system metadata for the object
             SystemMetadata m = cn.getSystemMetadata(token, id);
             if (m != null) {
-                setSystemMetadata(m);
+                o = new D1Object();
+                o.setSystemMetadata(m);
             }
             
             // Resolve the MNs that contain the object
@@ -263,7 +244,7 @@ public class D1Object {
                 try {
                     InputStream is = mn.get(token, id);
                     try {
-                        setData(IOUtils.toByteArray(is));
+                        o.setData(IOUtils.toByteArray(is));
                         gotData = true;
                         break;
                     } catch (IOException e) {
@@ -299,6 +280,8 @@ public class D1Object {
         } catch (NotImplemented e) {
             e.printStackTrace();
         }
+        
+        return o;
     }
 
     /**
@@ -308,18 +291,17 @@ public class D1Object {
      * @param format the format of the object
      * @param submitter the submitter for the object
      * @param nodeId the identifier of the node on which the object will be created
-     * @param describes the list of identifiers that this object describes
-     * @param describedBy the list of objects described by this object
      * @return the generated SystemMetadata instance
      * @throws NoSuchAlgorithmException if the checksum algorithm does not exist
      * @throws IOException if the data bytes can not be read
      * @throws NotFound 
+     * @throws InvalidRequest 
      */
     private SystemMetadata generateSystemMetadata(Identifier id, byte[] data, 
-    		String format, String submitter, String nodeId, String[] describes, 
-    		String[] describedBy) throws NoSuchAlgorithmException, IOException, NotFound {
+    		String format, String submitter, String nodeId) 
+            throws NoSuchAlgorithmException, IOException, NotFound, InvalidRequest {
 
-    	//            validateRequest(id, data, format, submitter, nodeId, describes, describedBy);
+    	validateRequest(id, data, format, submitter, nodeId);
 
     	SystemMetadata sm = new SystemMetadata();
     	sm.setIdentifier(id);
@@ -347,11 +329,12 @@ public class D1Object {
     	//set the size
     	sm.setSize(new BigInteger(String.valueOf(data.length)));
 
-    	//submitter
+    	// set submitter and rightholder from the associated string
     	Subject p = new Subject();
     	p.setValue(submitter);
     	sm.setSubmitter(p);
     	sm.setRightsHolder(p);
+    	
     	Date dateCreated = new Date();
     	sm.setDateUploaded(dateCreated);
     	Date dateUpdated = new Date();
@@ -363,29 +346,25 @@ public class D1Object {
     	sm.setOriginMemberNode(nr);
     	sm.setAuthoritativeMemberNode(nr);
 
-//TODO reimplement when the OAI-ORE ResourceMap becomes available    	
-/*
-    	// Describes and describedBy
-    	for (String describesId : describes) {
-    		Identifier dId = new Identifier();
-    		dId.setValue(describesId);
-    		sm.addDescribe(dId);
-    	}
-    	for (String describedById : describedBy) {
-    		Identifier dId = new Identifier();
-    		dId.setValue(describedById);
-    		sm.addDescribedBy(dId);
-    	}
-*/
-
     	return sm;
     }
 
-    protected void validateRequest(Identifier id, byte[] data, String format, String submitter, 
-            String nodeId, String[] describes, String[] describedBy) throws InvalidRequest {
+    /**
+     * 
+     * Check the given set of input arguments that they are all valid and not null, 
+     * and that strings are of non-zero length. 
+     * @param id
+     * @param data
+     * @param format
+     * @param submitter
+     * @param nodeId
+     * @throws InvalidRequest
+     */
+    protected static void validateRequest(Identifier id, byte[] data, String format, String submitter, 
+            String nodeId) throws InvalidRequest {
 
         List<Object> objects = Arrays.asList((Object)id, (Object)data, (Object)format, (Object)submitter, 
-                (Object)nodeId, (Object)describes, (Object)describedBy);
+                (Object)nodeId);
         D1Object.checkNotNull(objects);
         List<String> strings = Arrays.asList(id.getValue(), format.toString(), submitter, nodeId);
         D1Object.checkLength(strings);        
@@ -404,6 +383,11 @@ public class D1Object {
         }
     }
     
+    /**
+     * Check that the all strings in the array have > 0 length.
+     * @param strings
+     * @throws InvalidRequest
+     */
     protected static void checkLength(List<String> strings) throws InvalidRequest {
         for (String string : strings) {
             if (string.length() < 1) {
