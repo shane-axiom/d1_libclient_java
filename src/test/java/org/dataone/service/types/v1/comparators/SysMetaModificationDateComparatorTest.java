4cf5981f424e99c022897d2e5cccabb828acf5d9
Rob Nahf <rnahf@epscor.unm.edu>
2014-04-14 22:38:30 +0000

    Renamed SysMeta<Property>Comparators to follow full datatype naming conventions:  "SystemMetadata<Property>Comparator" - part 2 :-)

--- src/test/java/org/dataone/service/types/v1/comparators/SysMetaModificationDateComparatorTest.java
+++ src/test/java/org/dataone/service/types/v1/comparators/SysMetaModificationDateComparatorTest.java
@@ -69,7 +69,7 @@ public class SysMetaModificationDateComparatorTest {
         date3.setTime(200000000);
         meta3.setDateSysMetadataModified(date3);
         
-        SysMetaModificationDateComparator comparator = new SysMetaModificationDateComparator();
+        SystemMetadataDateModifiedComparator comparator = new SystemMetadataDateModifiedComparator();
         assertTrue("SysMetaModificationDateComparatorTest.testCompares - the meta1 should be less than meta2", 
         		comparator.compare(meta1, meta2) <0);
         
@@ -124,7 +124,7 @@ public class SysMetaModificationDateComparatorTest {
         date3.setTime(300000000);
         meta3.setDateSysMetadataModified(date3);
         
-        SysMetaModificationDateComparator comparator = new SysMetaModificationDateComparator();
+        SystemMetadataDateModifiedComparator comparator = new SystemMetadataDateModifiedComparator();
         assertTrue("SysMetaModificationDateComparatorTest.testCompares - is transitive", 
         		comparator.compare(meta1, meta2) < 0 && comparator.compare(meta2, meta3) < 0
         		&& comparator.compare(meta1,meta3) < 0);
@@ -164,7 +164,7 @@ public class SysMetaModificationDateComparatorTest {
         meta5.setDateSysMetadataModified(date5);
         
         
-        SysMetaModificationDateComparator comparator = new SysMetaModificationDateComparator();
+        SystemMetadataDateModifiedComparator comparator = new SystemMetadataDateModifiedComparator();
         
         assertTrue("meta1 and meta2 should be equal", comparator.compare(meta1, meta2) == 0);
         
