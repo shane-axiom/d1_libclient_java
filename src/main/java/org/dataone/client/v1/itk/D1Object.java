7202c41e71cf2678b612db413d3ec966ef22d42d
Andrei Buium <andreib@dataone.unm.edu>
2015-09-17 16:32:16 +0000

    Reverted last change that set the submitter to the test object since it was in a libclient class. Moved it up into the call in ContextAwareTestCaseDataone

--- src/main/java/org/dataone/client/v1/itk/D1Object.java
+++ src/main/java/org/dataone/client/v1/itk/D1Object.java
@@ -507,10 +507,8 @@ public class D1Object {
         	sm.setSerialVersion(BigInteger.ONE);
         	
         	// set rightsHolder from the associated string
-        	if (rightsHolder != null) {
+        	if (rightsHolder != null)
         		sm.setRightsHolder(rightsHolder);
-        		sm.setSubmitter(rightsHolder);
-        	}
         	
         	Date dateCreated = new Date();
         	sm.setDateUploaded(dateCreated);
