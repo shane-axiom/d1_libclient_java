b4adf1021db861f9adc4eed6a2f26a2415765877
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2015-12-03 00:31:19 +0000

    rollback libclient change that caught exception when parsing non-DN. Calling libs continue to catch the exception. This allows us to not release an update to d1_libclient_java. https://redmine.dataone.org/issues/7502

--- src/main/java/org/dataone/client/auth/CertificateManager.java
+++ src/main/java/org/dataone/client/auth/CertificateManager.java
@@ -562,29 +562,18 @@ public class CertificateManager extends Observable {
     }
 
     /**
-     * Returns D1-wide consistent Subject string representations
-     * If it is not a DN, the original string is returned rather
-     * than throwing an error.
+     * Returns D1-wide consistent Subject DN string representations
      * @see http://www.ietf.org/rfc/rfc2253.txt
-     * @param name - the subject representation
-     * @return the standard D1 representation, or the original string if not in DN format
+     * @param name - the [reasonable] DN representation
+     * @return the standard D1 representation
      */
     public String standardizeDN(String name) {
-        
-        String standardizedName = name;
-    	if (log.isDebugEnabled()) {
-            log.debug("name: " + name);    
-    	}
-    	try {
-	        X500Principal principal = new X500Principal(name);
-	        standardizedName = principal.getName(X500Principal.RFC2253);
-    	} catch (IllegalArgumentException iae) {
-            log.warn("name not a valid DN: " + name);
-    	}
-        if (log.isDebugEnabled()) {
+        if (log.isDebugEnabled())
+            log.debug("name: " + name);
+        X500Principal principal = new X500Principal(name);
+        String standardizedName = principal.getName(X500Principal.RFC2253);
+        if (log.isDebugEnabled())
             log.debug("standardizedName: " + standardizedName);
-        }
-        
         return standardizedName;
     }
 
